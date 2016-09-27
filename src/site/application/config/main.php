2a6402ac2740bf4bfed0880f53cba98d521dd8b0
The Zonebuilder <zone.builder@gmx.com>
2016-09-27 15:06:19 +0300

    Release 1.8.7

--- src/site/application/config/main.php
+++ src/site/application/config/main.php
@@ -2,10 +2,9 @@
 
 return array(
 	'title' => 'JUL Designer',
-	'version' => '1.8.5',
+	'version' => '1.8.7',
 	'zb_link' => 'http://www.google.com/search?hl=en&num=50&start=0&safe=0&filter=0&nfpr=1&q=The+Zonebuilder+web+development+programming+IT+society+philosophy+politics',
 	'work_dir' => DOCROOT.'assets',
-	//'ample_root' => 'https://googledrive.com/host/0B9zLzpZ-06uFY1A2OG5Dd0k3TW8/ample-sdk/ample/',
 	'ample_root' => Url::base(true).'amplesdk-mainta-0.9.4/ample/',
 	'jul_root' => '',
 );
